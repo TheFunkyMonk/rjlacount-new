504213b87a9b2b16f0e5f8e428717b035cd74989
R.J. LaCount <rjlacount@gmail.com>
2020-03-20 17:59:19 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	theme: {
+		fontFamily: {
+			sans: [
+				'Montserrat',
+				'sans-serif',
+			],
+			serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+			mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+		},
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+}
