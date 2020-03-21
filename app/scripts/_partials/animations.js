5ffe8a128f89a137c5861e8648c7c6341dca23d1
R.J. LaCount <rjlacount@gmail.com>
2020-03-21 14:11:54 -0700

    Add favicons and domready for waypoints

--- app/scripts/_partials/animations.js
+++ app/scripts/_partials/animations.js
@@ -1,3 +1,4 @@
+import domready from 'domready';
 import anime from 'animejs/lib/anime.es.js';
 import scrollTriggers from 'scroll-triggers';
 
@@ -5,9 +6,11 @@ const initScrollTriggers = () => {
 	const enterAnimate = document.querySelectorAll('.enter-animate');
 	const enterFade = document.querySelectorAll('.enter-fade');
 
+	const firstSection = document.querySelector('#section-one');
+	const four = document.querySelector('#intro-four');
+
 	[enterAnimate, enterFade].forEach((el) => {
-		anime({
-			targets: el,
+		anime.set(el, {
 			opacity: 0
 		});
 	});
@@ -19,10 +22,10 @@ const initScrollTriggers = () => {
 	scrollTriggers([
 		{
 			el: enterAnimate,
+			offset: 150,
 			inView: (el, options) => {
 				if (!el.classList.contains('entered')) {
-					anime({
-						targets: el,
+					anime.set(el, {
 						opacity: 1
 					});
 					anime({
@@ -43,16 +46,32 @@ const initScrollTriggers = () => {
 	scrollTriggers([
 		{
 			el: enterFade,
+			offset: 150,
 			inView: (el, options) => {
 				if (!el.classList.contains('entered')) {
 					anime({
 						targets: el,
 						easing: 'easeInOutQuart',
 						opacity: 1,
-						duration: 500
+						duration: 1000
 					});
 					el.classList.add('entered');
 				}
+			}
+		}
+	]);
+
+	scrollTriggers([
+		{
+			el: firstSection,
+			inView: (el, options) => {
+				anime({
+					targets: four,
+					easing: 'easeInOutQuart',
+					opacity: 0,
+					translateY: '-1rem',
+					duration: 500
+				});
 			}
 		}
 	]);
@@ -63,6 +82,7 @@ const initHeroAnimation = () => {
 	const two = document.querySelector('#intro-two');
 	const three = document.querySelector('#intro-three');
 	const four = document.querySelector('#intro-four');
+	const chevron = four.querySelector('svg');
 
 	[one, two, three].forEach(item => {
 		item.innerHTML = item.textContent.replace(/\S/g, '<span class=\'inline-block\'>$&</span>');
@@ -72,14 +92,13 @@ const initHeroAnimation = () => {
 		easing: 'easeInOutQuart'
 	});
 
+	anime.set([one, two, three, four], {
+		opacity: 0,
+	});
+
 	tl
 		.add({
-			targets: [one, two, three, four],
-			opacity: 0,
-			duration: 0
-		})
-		.add({
-			targets: [one],
+			targets: one,
 			opacity: 1,
 			duration: 0
 		})
@@ -92,7 +111,7 @@ const initHeroAnimation = () => {
 			delay: (el, i) => 70 * i
 		})
 		.add({
-			targets: [two],
+			targets: two,
 			opacity: 1,
 			duration: 0
 		})
@@ -105,28 +124,51 @@ const initHeroAnimation = () => {
 			delay: (el, i) => 70 * i
 		}, '+=500')
 		.add({
-			targets: [three],
-			opacity: 1,
-			duration: 0
-		})
-		.add({
-			targets: three.querySelectorAll('span'),
-			scale: ['0.5', 1],
-			opacity: [0, 1],
-			translateZ: 0,
-			duration: 300,
-			delay: (el, i) => 40 * i
-		}, '+=500')
-		.add({
-			targets: [four],
+			targets: [three, four],
 			opacity: 1,
 			duration: 1000
-		}, '-=1000')
+		}, '+=500')
+		.add({
+			targets: chevron,
+			translateY: '0.5rem',
+			easing: 'easeInQuart',
+			duration: 750
+		}, '+=500')
+		.add({
+			targets: chevron,
+			translateY: 0,
+			easing: 'easeOutQuart',
+			duration: 750
+		})
+		.add({
+			targets: chevron,
+			translateY: '0.5rem',
+			easing: 'easeInQuart',
+			duration: 500
+		}, '+=500')
+		.add({
+			targets: chevron,
+			translateY: 0,
+			easing: 'easeOutQuart',
+			duration: 250
+		})
+		.add({
+			targets: chevron,
+			translateY: '0.5rem',
+			easing: 'easeInQuart',
+			duration: 250
+		})
+		.add({
+			targets: chevron,
+			translateY: 0,
+			easing: 'easeOutQuart',
+			duration: 500
+		})
 }
 
 const initAnimations = () => {
 	initHeroAnimation();
-	initScrollTriggers();
+	domready(() => initScrollTriggers());
 }
 
 export { initAnimations }
